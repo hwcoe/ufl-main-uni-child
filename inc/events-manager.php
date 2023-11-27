834a35251de80d85480c433765607ea8e32e56b3
sezj <sarahzachrich@gmail.com>
2023-11-27 16:40:06 -0500

    Add event output condition filter for Events Manager

--- inc/events-manager.php
+++ inc/events-manager.php
@@ -42,3 +42,31 @@ function event_breadcrumb($post, $displayCurrent) {
 add_filter('breadcrumbs', 'breadcrumbs');
 endif;
 // Events Manager breadcrumb END
+
+ /**
+Events Manager Custom Event Attributes and Conditionals
+https://www.johnrussell.dev/blog/2014/07/events-manager-custom-event-attributes-conditionals/
+ */
+
+ function em_event_output_condition_filter($replacement, $condition, $match, $EM_Event){
+    // Checks for has_Host conditional
+    if(is_object($EM_Event) && preg_match('/^has_(Host)$/', $condition, $matches)){
+        if(array_key_exists($matches[1], $EM_Event->event_attributes) && !empty($EM_Event->event_attributes[$matches[1]]) ){
+            $replacement = preg_replace("/\{\/?$condition\}/", '', $match);
+        }else{
+            $replacement = '';
+        }
+    }
+
+    // Checks for no_Host conditional
+    if(is_object($EM_Event) && preg_match('/^no_(Host)$/', $condition, $matches)){
+        if(!array_key_exists($matches[1], $EM_Event->event_attributes) || empty($EM_Event->event_attributes[$matches[1]]) ){
+            $replacement = preg_replace("/\{\/?$condition\}/", '', $match);
+        }else{
+            $replacement = '';
+        }
+    }
+    
+    return $replacement;
+}
+add_filter('em_event_output_condition', 'em_event_output_condition_filter', 1, 4);
