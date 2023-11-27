6db25bc42470164721a1309db285fde29de46e4b
sezj <sarahzachrich@gmail.com>
2023-11-27 16:46:26 -0500

    Update single event template

--- single-event.php
+++ single-event.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Template Name: No Sidebar
- * Template Post Type: post
+ * Template Name: Event
+ * Template Post Type: event
  */
 
 get_header();  
@@ -9,37 +9,18 @@ get_header();
 event_breadcrumb($post, true);
 ?>
 
-<!-- <div id="content" class="site-content container py-5 mt-4"> -->
-  <!-- <div id="primary" class="content-area"> -->
 <main id="main" class="site-main container py-5 mt-4">
 	<header class="entry-header">
 		<?php the_post(); ?>
-		<?php bootscore_category_badge(); ?>
-		<h1><?php the_title(); ?></h1>
-		<p class="entry-meta">
-		 <small class="text-muted">
-		   <?php
-		     bootscore_date();
-		     bootscore_author();
-		     bootscore_comment_count();
-		   ?>
-		 </small>
-		</p>
-		<?php bootscore_post_thumbnail(); ?>
+		<h1><?php the_title(); ?></h1>	
 	</header>
 
 	<div id="content" class="single-news container py-5 mt-4">
-		<!-- Hook to add something nice -->
-		<?php bs_after_primary(); ?>
-
 		<div class="entry-content">
 			<?php the_content(); ?>
 		</div>
 	</div>
 	<footer class="entry-footer clear-both mt-5 mb-5 container">
-		<div class="mb-5 mt-3">
-			<?php bootscore_tags(); ?>
-		</div>
 		<nav aria-label="bS page navigation">
 			<ul class="pagination justify-content-center">
 				<li class="page-item">
@@ -50,11 +31,8 @@ event_breadcrumb($post, true);
 				</li>
 			</ul>
 		</nav>
-		<?php comments_template(); ?>
 	</footer>
       
-    </main>
- <!-- </div> -->
-<!-- </div> -->
+</main>
 <?php
 get_footer();
