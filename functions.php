a5d4f09b3ede77d2830efe8e99b65744aa4ffc79
sezj <sarahzachrich@gmail.com>
2023-11-28 10:23:47 -0500

    Move ACF PRO related functions to separate include file

--- functions.php
+++ functions.php
@@ -77,9 +77,12 @@ define( "HWCOE_CHILD_INC_DIR", get_stylesheet_directory() . "/inc" );
  * Load custom theme files 
  */
 require HWCOE_CHILD_INC_DIR . '/customizer.php';
+require HWCOE_CHILD_INC_DIR . '/template-tags.php';
+
+// Load plugin custom functions
+require HWCOE_CHILD_INC_DIR . '/acf-pro.php';
 require HWCOE_CHILD_INC_DIR . '/events-manager.php';
 require HWCOE_CHILD_INC_DIR . '/shibboleth.php';
-require HWCOE_CHILD_INC_DIR . '/template-tags.php';
 
 // delete footer nav menu location since the menu is not displayed by the theme
 if (!function_exists( 'hwcoe_unregister_footer_menu' )) {
@@ -88,52 +91,3 @@ if (!function_exists( 'hwcoe_unregister_footer_menu' )) {
 	}
 }
 add_action('init', 'hwcoe_unregister_footer_menu');
-
-/*
-* Integrating Advanced Custom Fields
-*/
-if (!function_exists('hwcoe_acf_json_save_point')) { 
-	add_filter('acf/settings/save_json', 'hwcoe_acf_json_save_point');
-
-	function hwcoe_acf_json_save_point( $path ) {
-		$path = HWCOE_CHILD_INC_DIR . '/acf-json';
-		// return
-		return $path; 
-	}
-}
-
-if (!function_exists('hwcoe_acf_json_load_point')) {
-	add_filter('acf/settings/load_json', 'hwcoe_acf_json_load_point');
-	function hwcoe_acf_json_load_point( $paths ) {	
-		unset($paths[0]);
-   
-		array_push(
-			$paths,
-			// load child theme custom fields
-			HWCOE_CHILD_INC_DIR . '/acf-json',
-			// load parent theme custom fields		
-			get_template_directory() . '/inc/advanced-custom-fields/acf-json'
-		);
-
-		return $paths;
-	}
-}
-
-// Limit the ACF Custom Fields dashboard menu to users who are site administrators (single site) or network admins (multisite)
-function hwcoe_acf_init() {
-	acf_update_setting('capability', 'update_plugins'); 
-}
-add_action('acf/init', 'hwcoe_acf_init');
-
-// remove parent theme's meta box to disable breadcrumbs since the value doesn't retain on save
-
-add_action('add_meta_boxes', 'remove_ufl_menu_metaBox', 9);
-function remove_ufl_menu_metaBox() {
-	remove_action( 'add_meta_boxes', 'ufl_menu_metaBox');
-}
-
-add_action('save_post', 'remove_ufl_save_menu_metaBox', 9);
-function remove_ufl_save_menu_metaBox() {
-	remove_action('save_post', 'ufl_save_menu_metaBox');
-}
-
